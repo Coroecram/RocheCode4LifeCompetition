a041914a7a4592f01e61ac55698f93c914db4a01
Michael Budnick <michaeldbudnick@gmail.com>
2017-05-15 17:55:08 -0400

    Build out action controller

--- ActionController.java
+++ ActionController.java
@@ -1,16 +1,53 @@
 import java.util.*; 
 
 public class ActionController {
-    Queue<String> actionQueue = new LinkedList<String>();
+    Robot robot = new Robot();
+    private List<Sample> allSamples;
+    private boolean started = true;
+    private Queue<String> actionQueue = new LinkedList<>();
     
-    public Queue<String> calculateNextActions(SampleHolder samples, MoleculeHolder molecules) {
-        if (samples.size == 0) {
-            
+    public ActionController(List<Sample> allSamples) {
+        this.setAllSamples(allSamples);
+    }
+    
+    public Queue<String> calculateNextActions(Robot robot) {
+        if (this.robot.getSamples().size() == 0) {
+            if (!this.robot.getLocation().equals("DIAGNOSIS")) {
+                this.actionQueue = new LinkedList<>();
+                goToDiagnosis();
+        
+            } 
         } else {
-            for (Sample sample : samples.getSamples()) {
-            
+            for (Sample sample : this.robot.getSamples().getSamples()) {
+                
             }
         }
-        return actionQueue;
+        
+        return this.actionQueue;
+    }
+
+    public List<Sample> getAllSamples() {
+        return this.allSamples;
+    }
+
+    public void setAllSamples(List<Sample> allSamples) {
+        this.allSamples = allSamples;
+    }
+    
+    private void goToDiagnosis() {
+        this.actionQueue.add("GOTO DIAGNOSIS");      
+    }
+    
+    private void goToLab() {
+        this.actionQueue.add("GOTO LABORATORY");   
+    }
+    
+    private void goToMolecules() {
+        this.actionQueue.add("GOTO MOLECULES");        
+    }
+    
+    private static String connect(String command) {
+        
+        return "CONNECT " + command;
     }
 }
--- Robot.java
+++ Robot.java
@@ -2,18 +2,38 @@
 public class Robot {
 
     private MoleculeHolder molecules;
-    private 
+    private SampleHolder samples;
+    private String location = "";
 
     public Robot() {
         this.setMolecules(new MoleculeHolder());
+        this.setSamples(new SampleHolder());
         
     }
-
-    private MoleculeHolder getMolecules() {
-        return molecules;
+    
+    public MoleculeHolder getMolecules() {
+        return this.molecules;
+    }
+    
+    public SampleHolder getSamples() {
+        return this.samples;
+    }
+    
+    public String getLocation() {
+        return this.location;
     }
 
-    private void setMolecules(MoleculeHolder molecules) {
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    
+    private
+    
+    void setSamples(SampleHolder sampleHolder) {
+        this.samples = sampleHolder;        
+    }
+
+    void setMolecules(MoleculeHolder molecules) {
         this.molecules = molecules;
     }
     
